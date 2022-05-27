cf5858f9620c1a8fa5bd92856231ee52e783c4a0
Lucia Nina <lucianina1994@gmail.com>
2022-05-27 12:22:00 -0300

    Se actualizo estilo del Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,59 @@ import { useCartContext } from "../../context/CartContext";
 import "./Cart.css";
 
 function Cart() {
-    const { cartList, clear } = useCartContext();
-    return (
-        <div>
-            {cartList.map((cartItem) => (
-                <div key={cartItem.id}>
-                    <h2>Producto: {cartItem.title}</h2>
-                    <h3>Cantidad: {cartItem.quantity}</h3>
-                    
-                </div>
+    const subtotalItem = (price, qty) => {
+        return price * qty;
+    };
 
-            ))}
-            <button className="btn" onClick={() => clear()}>Remover todos los items</button>
+    const total = (products) => {
+        return products.reduce((acc, cur) => acc + cur.quantity * cur.price, 0);
+    };
+
+    const { cartList, clear } = useCartContext();
+    console.log(cartList);
+    return (
+        <div className="small-container cart-page">
+            <table>
+                <tr>
+                    <th>Producto</th>
+                    <th>Cantidad</th>
+                    <th>Subtotal</th>
+                </tr>
+                {cartList.map((cartItem) => (
+                    <tr>
+                        <td>
+                            <div className="cart-info">
+                                <img src={cartItem.pictureUrl} alt="" />
+                                <div>
+                                    <p>{cartItem.title}</p>
+                                    <small>Price: $ {cartItem.price}</small>
+                                    <br />
+                                    <a href="#">Remove</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td>{cartItem.quantity}</td>
+                        <td>
+                            $ {subtotalItem(cartItem.price, cartItem.quantity)}
+                        </td>
+                    </tr>
+                ))}
+            </table>
+            <tfoot>
+                <tr>
+                    <button className="btn" onClick={() => clear()}>
+                        Vaciar el carrito
+                    </button>
+                </tr>
+            </tfoot>
+            <div className="total-price">
+                <table>
+                    <tr>
+                        <td>Total</td>
+                        <td>$ {total(cartList)}</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
